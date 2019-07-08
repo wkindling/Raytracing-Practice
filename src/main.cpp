b26fb4e2700f9760850c3dbdbb8a7c1717de8984
wkindling <422599446@qq.com>
2019-07-08 15:56:22 +0800

    chapter 4

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,23 @@
 
 using namespace std;
 
+bool hit_sphere(const Vec3& center, float radius, const Ray& r)
+{
+	Vec3 ac = r.origin() - center;
+	float a = r.direction().norm2();
+	float b = 2.0*r.direction().dot(ac);
+	float c = ac.norm2() - radius * radius;
+	float delta = b * b - 4 * a*c;
+	return (delta >= 0);
+}
+
 Vec3 color(const Ray& r)
 {
+	if (hit_sphere(Vec3(0, 0, -1), 0.5, r)) return Vec3(1, 0, 0);
 	Vec3 direction = r.direction().unit();
 	float t = 0.5*(direction.y() + 1.0);
 	return (1 - t)*Vec3(1, 1, 1) + t * Vec3(0.5, 0.7, 1.0);
 }
-
 
 int main()
 {
